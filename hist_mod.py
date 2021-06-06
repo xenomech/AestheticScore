0f8b58825c2a8368e462751d9153c852a6743d43
xenomech <gokulsmenon227@gmail.com>
2021-06-06 13:59:36 +0530

    ✨ Split

--- hist_mod.py
+++ hist_mod.py
@@ -6,8 +6,9 @@ from matplotlib import pyplot as plt
 # load an image in grayscale mode
 img = cv2.imread('ex.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2Luv)
-
 img = cv2.resize(img, (225, 225))
 cv2.imshow("frame",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+l, u, v = cv2.split(img)
